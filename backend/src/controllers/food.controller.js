e9a817e72adc50b064340b936898911a179cf8c5
Gautam vaishnav <gautamvaishnav0305@gmail.com>
2026-02-05 09:12:55 +0000

    postman

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,5 +1,6 @@
 const foodModel = require("../models/food.model");
-
+const storeAgeServices=require('../services/storage.service')
+const {v4:uuid}=require('uuid')
 exports.createFood = async (req, res) => {
-    console.log(req.body)
+   const fileUploadResult=await storeAgeServices.uploadFile(req.file.buffer,uuid())
 }
